6b5546bf8649ce456410846f062e7612119ef8d4
brenward <bren.ward@gmail.com>
2024-05-04 19:44:03 +0100

    #Using a subject to post an error message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -10,14 +11,16 @@ import { PostsService } from './posts.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts = [];
   isLoading = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postsService:PostsService) {}
-
+  
   ngOnInit() {
+    this.errorSub = this.postsService.error.subscribe(errorMessage => this.error = errorMessage);
     this.isLoading = true;
     this.postsService.fetchPosts().subscribe(posts =>{
       this.isLoading = false;
@@ -45,6 +48,10 @@ export class AppComponent implements OnInit {
     this.postsService.deletePosts().subscribe(()=>{
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -2,10 +2,12 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Injectable({providedIn:'root'})
 export class PostsService{
     firebaseUrl:string = '';
+    error = new Subject<string>();
 
     constructor(private http:HttpClient){}
 
@@ -15,6 +17,9 @@ export class PostsService{
             postData
             ).subscribe(responseData => {
               console.log(responseData);
+            },
+            error =>{
+                this.error.next(error.message);
             });
     }
 
