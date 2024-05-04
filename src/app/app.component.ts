6d7a351197eae1c7fcdd68a9fb8db99baa8d6650
brenward <bren.ward@gmail.com>
2024-05-04 16:40:37 +0100

    'Transforming response from get request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -36,7 +38,19 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts(){
-    this.http.get(this.firebaseUrl).subscribe((posts) => {
+    this.http.get(this.firebaseUrl)
+    .pipe(
+      map((responseData) => { 
+        const postsArray = [];
+        for(const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+        return postsArray;
+      })
+    )
+    .subscribe((posts) => {
       console.log(posts);
     });
   }
